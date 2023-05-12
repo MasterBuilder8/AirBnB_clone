5cfd5cd6de8bc1f0fb227ed8f0b35130c458c19f
MasterBuilder8 <collins.snilloc9@gmail.com>
2023-05-12 06:57:04 -0700

    BaseModel

--- models/base_model.py
+++ models/base_model.py
@@ -14,3 +14,27 @@ class BaseModel:
     This is the Base Model that take care of the
     initialization, serialization and deserialization
     of the future instances"""
+    def __init__(self, *args, **kwargs):
+        """initializes the base  instance
+
+        Args:
+            - *args: list of arguments
+            - **kwargs: dict of key-values arguments
+        """
+
+        if kwargs is not None and kwargs != {}:
+            for key in kwargs:
+                if key == "created_at":
+                    self.__dict__["created_at"] = datetime.strptime(
+                            kwargs["created_at"], "%Y-%m-%dT%H:%M:%S.%f")
+                elif key == "update_at":
+                    self.__dict__["update_at"] = datetime.strptime(
+                            kwargs["update_at"], "%Y-%m-%dT%H:%M:%S.%f")
+                else:
+                    self__dict__[key] = kwargs[key]
+                else:
+                    self.id = str(uuid.uuid4())
+                    self.created_at = datetime.now()
+                    self.updated_at = datetime.now()
+                    storage.new(self)
+
