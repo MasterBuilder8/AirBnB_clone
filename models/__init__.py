75dc54d8df0286e1364617efc06f159841d4d9d4
MasterBuilder8 <collins.snilloc9@gmail.com>
2023-05-16 04:08:32 -0700

    web

--- models/__init__.py
+++ models/__init__.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """Module that initializes storage variable"""
+
+
 from models.engine.file_storage import FileStorage
 
 
--- models/base_model.py
+++ models/base_model.py
@@ -1,61 +1,52 @@
 #!/usr/bin/python3
-"""The 'BaseModel' module defines a base class 'BaseModel' that 
-provides common attributes and methods that can be used by other classes in the application."""
+"""defining a base model superclass"""
 
 
 import uuid
 from datetime import datetime
-from models import  storage
+import models
 
 
 class BaseModel:
-    """Base Model Class
-
-    This is the Base Model that take care of the
-    initialization, serialization and deserialization
-    of the future instances"""
+    """Class BaseModel"""
     def __init__(self, *args, **kwargs):
-        """initializes the base  instance
+        """object instantiation
+        attributes: id
+                created-at
+                updated_at"""
+        if not kwargs:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = self.created_at
+            models.storage.new(self)
+        else:
+            format = "%Y-%m-%dT%H:%M:%S.%f"
+            for key, value in kwargs.items():
+                if key == 'created_at' or key == 'updated_at':
+                    self.__dict__[key] = datetime.strptime(value, format)
+                if key != '__class__':
+                    self.__dict__[key] = value
 
-        Args:
-            - *args: list of arguments
-            - **kwargs: dict of key-values arguments
-        """
-
-        if kwargs is not None and kwargs != {}:
-            for key in kwargs:
-                if key == "created_at":
-                    self.__dict__["created_at"] = datetime.strptime(
-                            kwargs["created_at"], "%Y-%m-%dT%H:%M:%S.%f")
-                elif key == "update_at":
-                    self.__dict__["update_at"] = datetime.strptime(
-                            kwargs["update_at"], "%Y-%m-%dT%H:%M:%S.%f")
-                else:
-                    self__dict__[key] = kwargs[key]
-                else:
-                    self.id = str(uuid.uuid4())
-                    self.created_at = datetime.now()
-                    self.updated_at = datetime.now()
-                    storage.new(self)
-     def __str__(self):
-        """Returns a human-readable string representation
-        of an instance."""
-
-        return "[{}] ({}) {}".\
-            format(type(self).__name__, self.id, self.__dict__)
+    def __str__(self):
+        """returns string representation of class name, id, and dictionary
+        ready to print"""
+        return '[' + str(self.__class__.__name__) + '] ('\
+            + str(self.id) + ') ' + str(self.__dict__)
 
     def save(self):
-        """Updates the updated_at attribute
-        with the current datetime."""
-
+        """ updates the public instance attribute updated_at with the
+        current datetime"""
         self.updated_at = datetime.now()
-        storage.save()
+        models.storage.save()
 
     def to_dict(self):
-        """Returns a dictionary representation of an instance."""
-
-        my_dict = self.__dict__.copy()
-        my_dict["__class__"] = type(self).__name__
-        my_dict["created_at"] = my_dict["created_at"].isoformat()
-        my_dict["updated_at"] = my_dict["updated_at"].isoformat()
-        return my_dict
+        """return python object"""
+        dictionary = self.__dict__.copy()
+        if 'created_at' in dictionary:
+            if isinstance(dictionary['created_at'], datetime):
+                dictionary['created_at'] = dictionary['created_at'].isoformat()
+        if 'updated_at' in dictionary:
+            if isinstance(dictionary['updated_at'], datetime):
+                dictionary['updated_at'] = dictionary['updated_at'].isoformat()
+        dictionary['__class__'] = self.__class__.__name__
+        return dictionary
